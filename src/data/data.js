65bca83251793a7efc64d0a944d35e51a2ba643a
Rodrigo Nikolof <rodrigo.nikolof@outlook.com>
2023-04-04 11:34:31 +1000

    Changed projects in data file

--- src/data/data.js
+++ src/data/data.js
@@ -1,20 +1,22 @@
 import WorkIm from '../assets/projects/workImg.jpeg'
 import RealEst from '../assets/projects/realestate.jpg'
+import CocktailsDB from '../assets/projects/cocktails.jpg'
+import Reviews from '../assets/projects/reviews.jpg'
 
 export const data=[
     {
         id:1,
         name:"React JS Application",
-        image:WorkIm,
-        github:"",
-        live:"",
+        image:CocktailsDB,
+        github:"https://github.com/rodrigonikolof/cocktails-db",
+        live:"https://rodrigonikolof.github.io/cocktails-db/",
     },
     {
         id:2,
         name:"React JS Application",
-        image:RealEst,
-        github:"",
-        live:"",
+        image:Reviews,
+        github:"https://github.com/rodrigonikolof/reviews-slider",
+        live:"https://rodrigonikolof.github.io/reviews-slider/",
     },
     {
         id:3,
