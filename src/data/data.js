dd2d5b0776962fc74193ee909c546f2068008a76
Rodrigo Nikolof <rodrigonikolof@outlook.com>
2023-04-27 09:02:06 +1000

    Updated data in data.js

--- src/data/data.js
+++ src/data/data.js
@@ -2,36 +2,31 @@ import WorkIm from '../assets/projects/workImg.jpeg'
 import RealEst from '../assets/projects/realestate.jpg'
 import CocktailsDB from '../assets/projects/cocktails.jpg'
 import Reviews from '../assets/projects/reviews.jpg'
+import Youdemy from '../assets/projects/youdemy.png'
 
 export const data=[
     {
         id:1,
-        name:"React JS Application",
-        image:CocktailsDB,
-        github:"https://github.com/rodrigonikolof/cocktails-db",
-        live:"https://rodrigonikolof.github.io/cocktails-db/",
+        name:"ReactJS Frontend + Ruby on Rails Backend",
+        image:Youdemy,
+        github:"https://github.com/rodrigonikolof/phase-4-project",
+        live:"https://phase-4-project-ft41.onrender.com/",
     },
     {
         id:2,
-        name:"React JS Application",
+        name:"ReactJS Application",
         image:Reviews,
         github:"https://github.com/rodrigonikolof/reviews-slider",
         live:"https://rodrigonikolof.github.io/reviews-slider/",
     },
     {
         id:3,
-        name:"React JS Application",
-        image:WorkIm,
-        github:"",
-        live:"",
+        name:"ReactJS Application",
+        image:CocktailsDB,
+        github:"https://github.com/rodrigonikolof/cocktails-db",
+        live:"https://rodrigonikolof.github.io/cocktails-db/",
     },
-    {
-        id:4,
-        name:"React JS Application",
-        image:RealEst,
-        github:"",
-        live:"",
-    },
+    
 
 
 ]
